3936ac3db24a8182df7bf537b020fc8b9cfa35f2
Angelica Garcia <a329528@uach.mx>
2021-05-02 20:11:33 -0600

    Servidor asíncrono que lea y envíe un archivo según su extensión

---
+++ asincrono_2.js
@@ -0,0 +1,19 @@
+const http= require('http');
+const fs= require('fs');
+
+//http => (request, response)
+http.createServer((request, response)=>{
+  console.log(request.url);
+  const file = request.url=='/'? './myfile.txt': `.${request.url}`;
+  fs.readFile(file, (err,data)=>{
+    if(err){
+      response.writeHead(400, {"Content.Type": "text/plain"});
+      response.write("not found");
+      response.end();
+    }else{
+      response.writeHead(200, {"Content.Type": "text/plain"});
+      response.write(data);
+      response.end();
+    }
+  });
+}).listen(4444);

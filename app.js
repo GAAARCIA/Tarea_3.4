e6a16483fd8cfcd35d9393d779f73a9e68b4797a
Angelica Garcia <a329528@uach.mx>
2021-05-06 22:03:22 -0600

    Se modifica app.js

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const http= require('http');
+const fs = require('fs');
+
+// http => (request, response)
+// const data = fs.readFileSync('./WWW/file.txt');
+
+// HTML CSS JS IMG AUDIO VIDEO
 
 http.createServer((request, response)=>{
-      response.writeHead(200, {"Content.Type": "text/plain"});
-      response.write("Hola mundo cruel");
+  console.log(request.url);
+  const file = request.url == '/' ? './WWW/file.txt' : `./WWW${request.url}`;
+  fs.readFile(file, (err,data)=>{
+    if(err){
+      response.writeHead(400, {"Content-Type" : "text/plain"});
+      response.write("Not Found");
       response.end();
+    }else{
+      response.writeHead(200, {"Content-Type" : "text/plain"});
+      response.write(data);
+      response.end();
+    }
+  });
 }).listen(4444);

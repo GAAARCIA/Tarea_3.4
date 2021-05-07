a53f7c843caef3b6fb9489c7001fe0894e06de3c
Angelica Garcia <a329528@uach.mx>
2021-05-06 22:24:09 -0600

    Se modifica app.js

--- app.js
+++ app.js
@@ -15,7 +15,27 @@ http.createServer((request, response)=>{
       response.write("Not Found");
       response.end();
     }else{
-      response.writeHead(200, {"Content-Type" : "text/plain"});
+      // "hola.como estas" => split(".").pop();
+      const extension = file.split('.').pop();
+      switch(extension){
+        case 'txt':
+          response.writeHead(200, {"Content-Type" : "text/plain"});
+          break;
+        case 'html':
+          response.writeHead(200, {"Content-Type" : "text/html"});
+          break;
+        case 'jpeg':
+          response.writeHead(200, {"Content-Type" : "image/jpeg"});
+          break;
+        case 'css':
+          response.writeHead(200, {"Content-Type" : "text/css"});
+          break;
+        case 'js':
+          response.writeHead(200, {"Content-Type" : "text/javascript"});
+          break;
+        default:
+          response.writeHead(200, {"Content-Type" : "text/plain"});
+      }
       response.write(data);
       response.end();
     }

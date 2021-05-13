9e6ccd2568d0597e078619c96a6fd5a157ed63c2
Angelica Garcia <a329528@uach.mx>
2021-05-13 15:03:17 -0600

    Se agrega nuevo archivo main.js

--- WWW/jss/main.js
+++ WWW/jss/main.js
@@ -1,2 +1,20 @@
 let input = document.getElementById("email");
 input.value = "a329528@uach.mx";
+
+
+let password = document.getElementById("password");
+let button = document.getElementById("show");
+let button2 = document.getElementById("hide");
+
+function mostrarPassword(){
+  let password = document.getElementById("pass");
+  password.type = 'text';
+}
+
+function ocultarPassword(){
+  let password = document.getElementById("pass");
+  password.type = 'password';
+}
+
+button.onclick = mostrarPassword;
+button2.onclick = ocultarPassword;
